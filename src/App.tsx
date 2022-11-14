fbf3c33a6c238b77581389caae1eb4e4551321af
Krystian Kościelniak <krystian@koscielniak.pro>
2022-11-14 13:40:58 +0100

    feat: Add connecting to the MetaMask wallet

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { createClient, WagmiConfig } from "wagmi";
+import { getDefaultProvider } from "ethers";
+import Profile from "./Profile";
+
+const client = createClient({
+  // autoConnect: true,
+  provider: getDefaultProvider(),
+});
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <WagmiConfig client={client}>
+      <Profile/>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </WagmiConfig>
   );
 }
 
