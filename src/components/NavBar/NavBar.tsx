17e7f5b796d60678885731eb0703f6972ac9224d
Krystian Kościelniak <krystian@koscielniak.pro>
2022-11-21 09:28:55 +0100

    feat: [NavBar] Add dividers

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,5 +1,5 @@
 import { GitHub } from "@mui/icons-material";
-import { Button, Tooltip } from "@mui/material";
+import { Box, Button, Divider, Tooltip } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
@@ -13,13 +13,25 @@ const NavBar = () => {
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           Stance Demo dApp
         </Typography>
-        <Tooltip title="Star me on GitHub">
-          <Button color="inherit" startIcon={<GitHub />}>
-            kkoscielniak/stance-dapp
-          </Button>
-        </Tooltip>
-        <PrizePool />
-        <WalletButton />
+        <Box
+          sx={{
+            display: "flex",
+            borderRadius: 1,
+            "& hr": {
+              mx: 0.5,
+            },
+          }}
+        >
+          <Tooltip title="Star me on GitHub">
+            <Button color="inherit" startIcon={<GitHub />}>
+              kkoscielniak/stance-dapp
+            </Button>
+          </Tooltip>
+          <Divider orientation="vertical" flexItem />
+          <PrizePool />
+          <Divider orientation="vertical" flexItem />
+          <WalletButton />
+        </Box>
       </Toolbar>
     </AppBar>
   );
